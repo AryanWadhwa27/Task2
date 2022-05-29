636520629a94c5ea555da49e7e11245dd94df390
AryanWadhwa27 <101052985+AryanWadhwa27@users.noreply.github.com>
2022-05-29 19:18:40 +0530

    Add files via upload

---
+++ Task2q2.py
@@ -0,0 +1,16 @@
+n=(14*51)/3
+'''
+n1=13
+N2=n1+P(N2)+Q(N2+1)
+n2=13+3/51+48/51(n2+1)
+'''
+print('to get two consecutive jacks the expected cards drawn is ',n)
+
+#part b
+n=(float(768/8))
+print ('number of cards = ',n)
+
+#part c
+
+print ('number of cards = ',13)
+
